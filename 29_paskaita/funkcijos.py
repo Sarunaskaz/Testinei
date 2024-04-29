27e643c95c110223ef71a3404a3e4b7e70249f52
Sarunaskaz <sarunas.kazakauskas2@gmail.com>
2024-04-29 14:47:50 +0300

    unitest

--- 29_paskaita/funkcijos.py
+++ 29_paskaita/funkcijos.py
@@ -9,3 +9,13 @@ def daugyba(a,b):
 
 def dalyba(a,b):
     return a / b
+
+def ar_lyginis(sk):
+    return sk % 2 == 0
+
+def suapvalinta_dalyba(a,b):
+    return round(a/b, 1)
+
+def prideti_i_zodyna(zodis):
+    zodynas = {'raktas': zodis}
+    return zodynas
